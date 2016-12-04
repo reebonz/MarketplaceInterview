316b18f3f97509c1fdfffe33d701f540b262c33d
johnjang <mhliu8@gmail.com>
2016-12-04 16:25:47 +0800

    [Part 1 - Coding] * It should start to get a discount at the second item. * Put discount message on the protal for user to know. * Format XML to make it more readable.

--- Marketplace.Interview/Marketplace.Interview.Business/Shipping/PerRegionDiscountShipping.cs
+++ Marketplace.Interview/Marketplace.Interview.Business/Shipping/PerRegionDiscountShipping.cs
@@ -10,21 +10,33 @@ namespace Marketplace.Interview.Business.Shipping
 
         public decimal SpecialRule_deducte { get; set; }
 
+        private bool isMultipleShipping { get; set; }
+
         public override string GetDescription(LineItem lineItem, Basket.Basket basket)
         {
-            return string.Format("Shipping to {0}", lineItem.DeliveryRegion);
+            return string.Format("Shipping to {0} {1}"
+                                , lineItem.DeliveryRegion
+                                , isMultipleShipping ? "with discount" : "");
         }
 
         public override decimal GetAmount(LineItem lineItem, Basket.Basket basket)
         {
             decimal result = 0m;
-            bool isMultipleShipping = false;
+            List<LineItem> foundItemList;
+            isMultipleShipping = false;
 
-            // Multiple rule: "with the same Shipping Option and the same Supplier and Region"
-            isMultipleShipping = basket.LineItems.Any(o =>  o.Id != lineItem.Id
-                                                            && o.Shipping is PerRegionDiscountShipping
+            #region Multiple rule: "if there is at least one other item in the basket with the same Shipping Option and the same Supplier and Region" 
+
+            foundItemList = basket.LineItems.FindAll(o =>  o.Shipping is PerRegionDiscountShipping
                                                             && o.SupplierId == lineItem.SupplierId
                                                             && o.DeliveryRegion == lineItem.DeliveryRegion);
+
+            // "at least one other item" && it should start to get a discount at the second item.
+            if (foundItemList.Count >= 2 && lineItem.Id != foundItemList[0].Id)
+            {
+                isMultipleShipping = true;
+            }
+            #endregion
 
             // Get Amount by mapping XML
             result = (  from c in PerRegionCosts
